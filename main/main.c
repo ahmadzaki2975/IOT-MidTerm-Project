7bbf790651bab82c06c982de949ed89c13ae1ec9
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-10-01 18:29:10 +0700

    feat: testing OLED display

--- main/main.c
+++ main/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "dht11.h"
+#include "ssd1306.h"
+
+#define I2C_MASTER_SCL_IO 22       /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21        /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUM_1    /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000   /*!< I2C master clock frequency */
+static ssd1306_handle_t ssd1306_dev = NULL;
 
 // ? Task untuk membaca sensor DHT11
-void DHT_Task() {
-  while(1) {
+void DHT_Task()
+{
+  while (1)
+  {
     double temp = DHT11_read().temperature;
     double hum = DHT11_read().humidity;
     printf("Temperature: %f\n", temp);
@@ -17,6 +27,27 @@ void DHT_Task() {
 
 void app_main(void)
 {
+  i2c_config_t conf;
+  conf.mode = I2C_MODE_MASTER;
+  conf.sda_io_num = (gpio_num_t)I2C_MASTER_SDA_IO;
+  conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.scl_io_num = (gpio_num_t)I2C_MASTER_SCL_IO;
+  conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+  conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+  conf.clk_flags = I2C_SCLK_SRC_FLAG_FOR_NOMAL;
+
+  i2c_param_config(I2C_MASTER_NUM, &conf);
+  i2c_driver_install(I2C_MASTER_NUM, conf.mode, 0, 0, 0);
+
+  ssd1306_dev = ssd1306_create(I2C_MASTER_NUM, SSD1306_I2C_ADDRESS);
+  ssd1306_refresh_gram(ssd1306_dev);
+  ssd1306_clear_screen(ssd1306_dev, 0x00);
+
+  char data_str[10] = {0};
+  sprintf(data_str, "C STR");
+  ssd1306_draw_string(ssd1306_dev, 70, 16, (const uint8_t *)data_str, 16, 1);
+  ssd1306_refresh_gram(ssd1306_dev);
+
   // ? Inisialisasi pin GPIO 32 untuk sensor DHT11
   DHT11_init(GPIO_NUM_32);
 
